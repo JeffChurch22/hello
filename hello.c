10fc07700e288fc78a8a6784600e7696bb22eb96
Jeffrey Church <jefflchurch@huskers.unl.edu>
2021-08-22 03:37:44 +0000

    actually added the code this time, and added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Jeffrey Church
+ * Date: 2021/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jeffrey Church!!!\n");
+
+  printf("Major: Computer Science\n");
+
+  return 0;
+}
